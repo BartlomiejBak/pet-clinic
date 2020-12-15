10be75d3d6b03a86326ba37fc9d4a14fb98400ba
BartlomiejBak <bakbartlomiej88@gmail.com>
2020-12-15 19:13:48 +0100

    refactor: refactor interface to common base interface. Closes #10

--- pet-clinic-data/src/main/java/pl/bartekbak/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/pl/bartekbak/petclinic/services/PetService.java
@@ -2,11 +2,6 @@ package pl.bartekbak.petclinic.services;
 
 import pl.bartekbak.petclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/pl/bartekbak/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/pl/bartekbak/petclinic/services/VetService.java
@@ -2,10 +2,5 @@ package pl.bartekbak.petclinic.services;
 
 import pl.bartekbak.petclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 }
