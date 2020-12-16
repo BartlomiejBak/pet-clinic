2c8a5c6525dbe8eb38601ec2fa5de5971256b9c2
BartlomiejBak <bakbartlomiej88@gmail.com>
2020-12-16 20:04:07 +0100

    feat: add Pet Clinic index page and controller. closes #7

---
+++ pet-clinic-web/src/main/java/pl/bartekbak/petclinic/controllers/indexController.java
@@ -0,0 +1,14 @@
+package pl.bartekbak.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class indexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index(){
+
+        return "index";
+    }
+}
