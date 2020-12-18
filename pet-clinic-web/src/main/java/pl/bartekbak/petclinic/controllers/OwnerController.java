7b1e68b6f2bac10fb3d06689b897ce21bd40813c
BartlomiejBak <bakbartlomiej88@gmail.com>
2020-12-18 20:06:40 +0100

    feat: add Owners index page and controller. closes #12

--- pet-clinic-web/src/main/java/pl/bartekbak/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/pl/bartekbak/petclinic/controllers/OwnerController.java
@@ -3,13 +3,13 @@ package pl.bartekbak.petclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/vets")
+@RequestMapping("/owners")
 @Controller
-public class VetController {
+public class OwnerController {
 
     @RequestMapping({"", "/index", "/index.html"})
     public String listVets() {
 
-        return "vets/index";
+        return "owners/index";
     }
 }
