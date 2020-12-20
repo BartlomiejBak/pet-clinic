389d6103ba921620d934bc7d008e565ab5d376c7
BartlomiejBak <bakbartlomiej88@gmail.com>
2020-12-20 14:54:16 +0100

    feat: list all owners on Vets Index page. Closes #14

--- pet-clinic-web/src/main/java/pl/bartekbak/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/pl/bartekbak/petclinic/controllers/VetController.java
@@ -1,14 +1,24 @@
 package pl.bartekbak.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.bartekbak.petclinic.services.VetService;
 
 @RequestMapping("/vets")
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"", "/index", "/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+
+        model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
     }
