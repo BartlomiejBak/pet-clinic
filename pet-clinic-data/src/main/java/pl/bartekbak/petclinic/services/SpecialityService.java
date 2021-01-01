d2c479608a18bc46c322b078eceadb6416d199a7
BartlomiejBak <bakbartlomiej88@gmail.com>
2021-01-01 12:51:12 +0100

    feat: create SpecialityServiceMap, closes #31

---
+++ pet-clinic-data/src/main/java/pl/bartekbak/petclinic/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package pl.bartekbak.petclinic.services;
+
+import pl.bartekbak.petclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/pl/bartekbak/petclinic/services/map/SpecialityServiceMap.java
@@ -0,0 +1,35 @@
+package pl.bartekbak.petclinic.services.map;
+
+import org.springframework.stereotype.Service;
+import pl.bartekbak.petclinic.model.Speciality;
+import pl.bartekbak.petclinic.services.SpecialityService;
+
+import java.util.Set;
+
+@Service
+public class SpecialityServiceMap extends  AbstractMapService<Speciality, Long> implements SpecialityService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.deleteByObject(object);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}
