0348e5adb83e1be70cee4f775dabc0d6c3213e86
BartlomiejBak <bakbartlomiej88@gmail.com>
2021-01-01 10:49:09 +0100

    feat: add contact info properties to Owner, closes #28

--- pet-clinic-data/src/main/java/pl/bartekbak/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/pl/bartekbak/petclinic/model/Owner.java
@@ -3,7 +3,34 @@ package pl.bartekbak.petclinic.model;
 import java.util.Set;
 
 public class Owner extends Person {
+    private String address;
+    private String city;
+    private String phone;
     private Set<Pet> pets;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 
     public Set<Pet> getPets() {
         return pets;
