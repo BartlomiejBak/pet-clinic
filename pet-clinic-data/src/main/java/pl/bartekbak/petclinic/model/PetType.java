2a46c492a259f3bfbd8d33db6e9cb446ae818f82
BartlomiejBak <bakbartlomiej88@gmail.com>
2021-01-01 11:13:01 +0100

    feat: add PetTypeService, closes #29

--- pet-clinic-data/src/main/java/pl/bartekbak/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/pl/bartekbak/petclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package pl.bartekbak.petclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
 
     private String name;
 
---
+++ pet-clinic-data/src/main/java/pl/bartekbak/petclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package pl.bartekbak.petclinic.services;
+
+import pl.bartekbak.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
