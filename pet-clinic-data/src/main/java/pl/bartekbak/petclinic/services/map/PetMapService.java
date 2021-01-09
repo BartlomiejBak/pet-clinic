ec1a3a9761f1fdb2fa435763cd4f71608bdc9ea1
BartlomiejBak <bakbartlomiej88@gmail.com>
2021-01-09 18:36:32 +0100

    fix: add Spring Data JPA service for speciality, closes #43

--- pet-clinic-data/src/main/java/pl/bartekbak/petclinic/services/map/PetMapService.java
+++ pet-clinic-data/src/main/java/pl/bartekbak/petclinic/services/map/PetMapService.java
@@ -7,7 +7,7 @@ import pl.bartekbak.petclinic.services.PetService;
 import java.util.Set;
 
 @Service
-public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
+public class PetMapService extends AbstractMapService<Pet, Long> implements PetService {
     @Override
     public Set<Pet> findAll() {
         return super.findAll();
