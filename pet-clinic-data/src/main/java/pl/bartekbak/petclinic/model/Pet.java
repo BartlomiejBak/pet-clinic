c3d7fa1faa64b2d2cfcb68e77e34ffb1f9866c74
BartlomiejBak <bakbartlomiej88@gmail.com>
2021-01-26 08:42:49 +0100

    feat: refactor builder, #56

--- pet-clinic-data/src/main/java/pl/bartekbak/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/pl/bartekbak/petclinic/model/Pet.java
@@ -11,13 +11,13 @@ import java.util.Set;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 @Entity
 @Table(name = "pets")
 public class Pet extends BaseEntity {
 
     @Column(name = "name")
     private String name;
+
 
     @ManyToOne
     @JoinColumn(name = "type_id")
@@ -32,5 +32,18 @@ public class Pet extends BaseEntity {
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
     private Set<Visit> visits = new HashSet<>();
+
+    @Builder
+    public Pet(Long id, String name, PetType petType, Owner owner, LocalDate birthday, Set<Visit> visits) {
+        super(id);
+        this.name = name;
+        this.petType = petType;
+        this.owner = owner;
+        this.birthday = birthday;
+
+        if (visits == null || visits.size() > 0 ) {
+            this.visits = visits;
+        }
+    }
 
 }
